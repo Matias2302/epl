afe849013b9896a98580e83f6b1a00e6fa1549f4
Matias Maciel <mmaciel@aacademica13.ADMINISTRATIVO>
2018-06-05 16:12:08 -0300

    antes de romperlo

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ var chart = new Chart(ctx, {
     data: {
         labels: ["1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"],
         datasets: [{
-            label: "My First dataset",
+            label: "Total población privada de libertad",
 
             borderColor: 'rgb(255, 99, 132)',
             data: [4117, 4369, 5042, 5912, 6903, 7139, 7004, 6789, 7213, 7739, 8324, 8775, 9185, 9413, 9688, 9727, 9937, 10303, 11005],
@@ -14,8 +14,18 @@ var chart = new Chart(ctx, {
     },
 
     // Configuration options go here
-    options: {}
+    options: {
+        layout:{
+            padding:{
+                left:50,
+                right:50,
+                top:50,
+                bottom:50
+            }
+        }
+    }
 });
+
 $('#chart_1').click(function(){
     if($('#charts').css('display')== 'none'){
         $('#charts').show();
